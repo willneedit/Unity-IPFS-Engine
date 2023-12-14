5184f28aa22a172246f5fd504df45a3a832675b1
willneedit <willneedit@nurfuerspam.de>
2023-12-14 13:57:11 +0100

    Added net-ipfs-core and its dependencies (as references)

---
+++ Assets/IPFS_Engine/ipfs.core/ProtobufHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+
+namespace Ipfs
+{
+    static class ProtobufHelper
+    {
+        static MethodInfo writeRawBytes = typeof(CodedOutputStream)
+            .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Single(m =>
+                m.Name == "WriteRawBytes" && m.GetParameters().Count() == 1
+            );
+        static MethodInfo readRawBytes = typeof(CodedInputStream)
+            .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Single(m =>
+                m.Name == "ReadRawBytes"
+            );
+
+        public static void WriteSomeBytes(this CodedOutputStream stream, byte[] bytes)
+        {
+            writeRawBytes.Invoke(stream, new object[] { bytes });
+        }
+
+        public static byte[] ReadSomeBytes(this CodedInputStream stream, int length)
+        {
+            return (byte[])readRawBytes.Invoke(stream, new object[] { length });
+        }
+    }
+}
