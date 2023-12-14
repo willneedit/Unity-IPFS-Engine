107423490e018960031df96eb07683447068be5d
willneedit <willneedit@nurfuerspam.de>
2023-12-14 21:15:16 +0100

    Added net-ipfs-core, net-ipfs-engine and net-peer-talk with NuGet helpers

---
+++ Assets/IPFS_Engine/ipfs.engine/Cryptography/KeyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipfs.Engine.Cryptography
+{
+    class KeyInfo : IKey
+    {
+        public string Name { get; set; }
+        public MultiHash Id { get; set; }
+    }
+}
---
+++ PluginsGen/ipfs.engine-Plugins/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipfs.engine_Plugins
+{
+    public class Class1
+    {
+    }
+}
